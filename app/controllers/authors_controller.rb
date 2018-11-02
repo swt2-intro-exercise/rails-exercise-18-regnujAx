16ba70e31c851b950c2b0008624161c143b80851
regnujAx <alexander.junger@student.hpi.uni-potsdam.de>
2018-11-02 15:12:12 +0000

    Fixed issue #3

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -5,7 +5,20 @@ class AuthorsController < ApplicationController
   def index
   end
 
-  def create
-  	render plain: params[:author].inspect
+  def show
+    @author = Author.find(params[:id])
   end
+
+  def create
+  	#render plain: params[:author].inspect
+  	@author = Author.new(author_params)
+ 
+  	@author.save
+  	redirect_to @author
+  end
+
+  private
+  	def author_params
+  		params.require(:author).permit(:first_name, :last_name, :homepage)
+  	end
 end
