6564f2403ead716fcd3a023f8c9606e6a237bf22
regnujAx <alexander.junger@student.hpi.uni-potsdam.de>
2018-11-08 17:11:06 +0000

    Fixed issue #11 - second try

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -32,8 +32,15 @@ class AuthorsController < ApplicationController
     	redirect_to @author
   	else
     	render 'edit'
+  	end
   end
-end
+
+  def destroy
+  	@author = Author.find(params[:id])
+  	@author.destroy
+ 
+  	redirect_to authors_path
+  end
 
   private
   	def author_params
